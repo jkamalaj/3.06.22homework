10db512e77c7922528bb762265cf0afadf969b9f
kamalagj <kamalagj@code.edu.az>
2022-06-03 11:46:42 +0400

    file added

---
+++ Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1.Class
+{
+    internal class Department
+    {
+        public string Name;
+        public int Employeelimit;
+        public int Salarylimit;
+        public string Employees;
+        public string AddEmployess;
+
+
+
+    }
+}
---
+++ Employee.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1.Class
+{
+    internal class Employee:Department
+    {
+        private string _name;
+        private string _surname;
+        private int _salary;
+        public Employee[] employees = new Employee[0];
+        public string Name 
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                for (int i = 0; i < value.Length; i++)
+                    if (!string.IsNullOrEmpty(value) && Char.IsUpper(value[0]))
+
+                           _name = value;
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+            set
+            {
+                for (int i = 0; i < value.Length; i++)
+                    if (!string.IsNullOrEmpty(value) && Char.IsUpper(value[i]))
+                        _surname = value;
+            }
+        }
+        public int Salary
+        {
+            get
+            {
+                return _salary;
+            }
+            set
+            {
+                if (value > 250)
+                    _salary = value;
+            }
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            if(Employees.Length < Employeelimit)
+            {
+                Array.Resize(ref employees , Employees.Length + 1);
+                employees[Employees.Length - 1] = employee;
+            }
+        }
+
+    }
+}
